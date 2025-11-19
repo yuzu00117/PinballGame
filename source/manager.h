cf9174bda707936d839959d061b3cbe8112a8b6e
yuzu00117 <66988732+yuzu00117@users.noreply.github.com>
2025-11-19 17:22:48 +0900

    コメント修正

--- source/manager.h
+++ source/manager.h
@@ -6,12 +6,23 @@
 
 class Manager {
 public:
+    // ----------------------------------------------------------------------
+    // 関数定義
+    // ----------------------------------------------------------------------
+    /// <summary>
+    /// ライフサイクルメソッド
+    /// </summary>
     static void Init();
     static void Uninit();
     static void Update();
     static void Draw();
 
+    // WHY: usingって変数？
 	using Scene = ::Scene;
+
+    /// <summary>
+    /// シーン変更
+    /// </summary>
     static void ChangeScene(Scene newScene);
 
 	// 現在のシーンの GameObjectを取得
@@ -20,6 +31,9 @@ public:
     }
 
 private:
+    // ----------------------------------------------------------------------
+    // 変数定義
+    // ----------------------------------------------------------------------
     static Scene m_CurrentScene;
     static std::vector<GameObject*> m_SceneGameObjects;
 };
