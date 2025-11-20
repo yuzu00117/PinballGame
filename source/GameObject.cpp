ad66bf64529ec02046c0ac4c0436f665e2efc1f7
yuzu00117 <yuzu00117.ponzoo@gmail.com>
2025-11-21 03:14:37 +0900

    GameObjectクラスをリファクタリング

--- source/GameObject.cpp
+++ source/GameObject.cpp
@@ -2,7 +2,79 @@
 #include "ColliderGroup.h"
 #include "Collider.h"
 
-// 再帰的に自分と子オブジェクトンのコライダーを取得
+// ------------------------------------------------------------------------------
+// 更新処理
+// ------------------------------------------------------------------------------
+void GameObject::Update()
+{
+    // コンポーネントの更新
+    for (auto& Component : m_Components) Component->Update();
+    // 子オブジェクトの更新
+    for (auto& Child : m_Children) Child->Update();
+}
+
+// ------------------------------------------------------------------------------
+// 描画処理
+// ------------------------------------------------------------------------------
+void GameObject::Draw()
+{
+    // コンポーネントの描画
+    for (auto &Component : m_Components)
+        Component->Draw();
+    // 子オブジェクトの描画
+    for (auto &Child : m_Children)
+        Child->Draw();
+
+    // デバッグ用コライダー描画
+    if (g_EnableColliderDebugDraw)
+    {
+        for (auto &component : m_Components)
+        {
+            if (auto collider = dynamic_cast<Collider *>(component.get()))
+            {
+                collider->DebugDraw();
+            }
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------
+// 子オブジェクトを追加する
+// ------------------------------------------------------------------------------
+GameObject* GameObject::CreateChild()
+{
+    auto child = std::make_unique<GameObject>();
+    GameObject *ptr = child.get();
+    AttachChild(std::move(child));
+    return ptr;
+}
+
+// ------------------------------------------------------------------------------
+// 子オブジェクトをアタッチする
+// ------------------------------------------------------------------------------
+void GameObject::AttachChild(std::unique_ptr<GameObject> child)
+{
+    child->m_Parent = this;
+    child->m_Transform.SetParent(&m_Transform);
+    m_Children.push_back(std::move(child));
+}
+
+// ------------------------------------------------------------------------------
+// 子オブジェクトをデタッチする
+// ------------------------------------------------------------------------------
+void GameObject::DetachAllChildren()
+{
+    for (auto &Child : m_Children)
+    {
+        Child->m_Transform.ClearParent();
+        Child->m_Parent = nullptr;
+    }
+    m_Children.clear();
+}
+
+// ------------------------------------------------------------------------------
+// 再帰的に自分と子オブジェクトのコライダーを取得
+// ------------------------------------------------------------------------------
 void GameObject::CollectCollidersRecursive(std::vector<Collider*>& outColliders)
 {
     // 自分のコンポーネント
