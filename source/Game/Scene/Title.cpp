ec7a2377e302cc8cd2eb0c91106d0da2e9d08f25
yuzu00117 <66988732+yuzu00117@users.noreply.github.com>
2026-01-29 12:08:43 +0900

    ファイルを修正

--- source/Game/Scene/Title.cpp
+++ source/Game/Scene/Title.cpp
@@ -1,23 +1,32 @@
 ﻿// title.cpp
-#include "main.h"
 #include "title.h"
+
+// システム
+#include "main.h"
 #include "renderer.h"
+
+// Windows API
 #include <windows.h>
 
+// 初期化処理
 void Title::Init()
 {
 }
 
+// 終了処理
 void Title::Uninit()
 {
 }
 
+// 更新処理
 void Title::Update(float deltaTime)
 {
 }
 
+// 描画処理
 void Title::Draw()
 {
+    // タイトル表示テキスト
     Renderer::DrawText(L"=== PinBall Battle ===", 100, 200);
     Renderer::DrawText(L"Press Enter to Start", 100, 240);
 }
